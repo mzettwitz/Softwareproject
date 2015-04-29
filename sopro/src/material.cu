f57e3cab5c42e1f6ae20b9da357e7ce76ce59fe3
Martin Zettwitz <martin.zettwitz@st.ovgu.de>
2015-04-29 20:42:22 +0200

    updated material and CMakeList (reabase)

--- sopro/src/material.cu
+++ sopro/src/material.cu
@@ -4,4 +4,22 @@
  *
  */
 
+#include "sopro/include/material.h"
 
+Material::Material(){
+    this->color = float4(0,0,0,1);
+}
+
+Material::Material(float4 color){
+    this->color = color;
+}
+
+Material::~Material(){}
+
+void Material::setColor(float4 newColor){
+    this->color = newColor;
+}
+
+float4 Material::getColor(){
+    return this->color;
+}
